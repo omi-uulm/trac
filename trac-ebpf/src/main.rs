a42d29764174a67eb38e773a2870957525c3138a
Sascha Winkelhofer <sascha.winkelhofer@gini.net>
2023-12-17 00:45:55 +0100

    cycles count

--- trac-ebpf/src/main.rs
+++ trac-ebpf/src/main.rs
@@ -1,18 +1,42 @@
 #![no_std]
 #![no_main]
 
-mod vmlinux;
-mod bpf_perf_event_data;
+mod task;
+mod perf_event;
 
-use core::{panic, ptr::{null, null_mut}, default, mem::size_of};
 use aya_bpf::{
-    helpers::{bpf_get_smp_processor_id, bpf_perf_prog_read_value, bpf_get_current_comm, bpf_get_current_task, bpf_get_current_task_btf, bpf_perf_event_output, bpf_perf_event_read},
-    macros::{perf_event, tracepoint},
-    programs::{ perf_event, PerfEventContext, TracePointContext},
-    BpfContext, bindings::bpf_perf_event_value,
+    helpers::{ bpf_get_smp_processor_id, bpf_perf_prog_read_value, bpf_get_current_comm, bpf_get_current_task_btf, bpf_ktime_get_tai_ns, bpf_ktime_get_boot_ns },
+    macros::{ perf_event, tracepoint, map },
+    bindings::{ bpf_perf_event_value, bpf_perf_event_data },
+    programs::{ PerfEventContext, TracePointContext },
+    BpfContext,
+    maps::{ PerCpuHashMap, Array, HashMap },
 };
-use aya_log_ebpf::{info};
-use vmlinux::{task_struct};
+use aya_log_ebpf::info;
+use core::{
+    str::from_utf8_unchecked,
+    ptr::addr_of_mut,
+};
+use task::task_struct;
+use perf_event::bpf_perf_event_data as native_bpf_perf_event_data;
+
+pub struct ProcessPerfCounterEntry {
+    pub prev_counter: u64,
+    pub proc_counter: u64,
+}
+
+static START_TIME_KEY: u64 = 0;
+static SAMEPLE_RATE_KEY: u64 = 1;
+static MS_IN_NS: u64 = 1000000;
+
+#[map]
+static SETTINGS_MAP: HashMap<u64, u64> = HashMap::with_max_entries(2, 0);
+
+#[map]
+static PROC_MAP: PerCpuHashMap<u64, ProcessPerfCounterEntry> = PerCpuHashMap::with_max_entries(1024, 0);
+
+#[map]
+static TOTAL_CYCLES_MAP: Array<u64> = Array::with_max_entries(262144, 0);
 
 #[perf_event]
 pub fn observe_cpu_clock(ctx: PerfEventContext) -> u32 {
@@ -22,34 +46,69 @@ pub fn observe_cpu_clock(ctx: PerfEventContext) -> u32 {
     }
 }
 
+fn get_current_bucket() -> u32 {
+    let mut current_bucket = 0;
+    let timestamp = unsafe { bpf_ktime_get_boot_ns() };
+
+    match unsafe { SETTINGS_MAP.get(&START_TIME_KEY) } {
+        None => {
+            match SETTINGS_MAP.insert(&START_TIME_KEY, &timestamp, 0) {
+                Ok(_) => {},
+                Err(_) => { return current_bucket }
+            }
+        },
+        Some(i) => {
+            // TODO: replace 500 by sample rate
+            current_bucket = ((timestamp - i) / MS_IN_NS / 500) as u32;
+        }
+    }
+
+    current_bucket
+}
+
 fn try_observe_cpu_clock(ctx: PerfEventContext) -> Result<u32, u32> {
-    let pid = ctx.pid() as i32;
-    let cpu = unsafe { bpf_get_smp_processor_id() };
-    let prog_name =  bpf_get_current_comm().map_err(|e| e as u32)?;
-    let prog_name = unsafe {core::str::from_utf8_unchecked(&prog_name)};
-
+    // let cpu = unsafe { bpf_get_smp_processor_id() };
+    // let prog_name =  bpf_get_current_comm().map_err(|e| e as u32)?;
+    // let prog_name = unsafe { from_utf8_unchecked(&prog_name) };
     let mut task: *mut task_struct = unsafe { bpf_get_current_task_btf() as *mut task_struct };
+    let read_value_ctx: *mut native_bpf_perf_event_data = ctx.as_ptr() as *mut native_bpf_perf_event_data;
+    let mut value = bpf_perf_event_value { counter: 0, enabled: 0, running: 0 };
+    let value_ptr = addr_of_mut!(value);
 
-    let mut ev: *mut bpf_perf_event_data::bpf_perf_event_data = unsafe { ctx.as_ptr() as *mut bpf_perf_event_data::bpf_perf_event_data };
-
-    let mut v = bpf_perf_event_value {
-        counter: 0,
-        enabled: 0,
-        running: 0,
-    };
-    let mut value = core::ptr::addr_of_mut!(v);
-
-    unsafe { bpf_perf_prog_read_value(ev as *mut aya_bpf::bindings::bpf_perf_event_data, value, core::mem::size_of::<bpf_perf_event_value>() as u32); }
+    unsafe { bpf_perf_prog_read_value(read_value_ctx as *mut bpf_perf_event_data, value_ptr, core::mem::size_of::<bpf_perf_event_value>() as u32); }
 
     for _ in 1..8 {
-        match unsafe { (*task).pid } {
+        let cur_pid = unsafe { (*task).pid } as u64;
+        match cur_pid {
             0 => break,
             1 => break,
             10202 => {
-                info!(&ctx, "pid: {}", pid);
-                info!(&ctx, "cpu: {}", cpu);
-                info!(&ctx, "prog_name: {}", prog_name);
-                info!(&ctx, "valuee: {}", unsafe { (*value).counter });
+                let counter = value.counter;
+                let mut proc_entry = ProcessPerfCounterEntry{prev_counter: counter, proc_counter: 0};
+                let mut additional_cycles: u64 = 0;
+                let current_bucket = get_current_bucket();
+
+                match unsafe { PROC_MAP.get(&cur_pid) } {
+                    None => {},
+                    Some(i) => {
+                        additional_cycles = counter - i.prev_counter;
+                        proc_entry.proc_counter = i.proc_counter + additional_cycles;
+                        proc_entry.prev_counter = counter;
+                    }
+                }
+                let _ = PROC_MAP.insert(&cur_pid, &proc_entry, 0);
+
+                match TOTAL_CYCLES_MAP.get_ptr_mut(current_bucket) {
+                    None => {},
+                    Some(i) => {
+                        unsafe { *i += additional_cycles };
+                    }
+                }
+
+                // info!(&ctx, "prog_name: {}", prog_name);
+                // info!(&ctx, "pid: {}", cur_pid);
+                // info!(&ctx, "cpu: {}", cpu);
+                // info!(&ctx, "valuee: {}", proc_entry.proc_counter);
                 break;
             }
             _ => {}
--- trac/src/main.rs
+++ trac/src/main.rs
@@ -1,10 +1,42 @@
+use aya::maps::{Array, MapData, HashMap};
 use aya::programs::perf_event::{perf_hw_id, PerfEventScope};
 use aya::programs::{PerfEvent, PerfTypeId, SamplePolicy, TracePoint};
 use aya::util::online_cpus;
 use aya::{include_bytes_aligned, Bpf};
 use aya_log::BpfLogger;
+use libc::__c_anonymous_ptrace_syscall_info_entry;
 use log::{debug, info, warn};
 use tokio::signal;
+use std::{
+    borrow::BorrowMut,
+    time::Instant,
+    fs::read_to_string,
+};
+use scanf::sscanf;
+
+static START_TIME_KEY: u64 = 0;
+static SAMEPLE_RATE_KEY: u64 = 1;
+
+pub fn boot_time_get_ns() -> Result<u64, u64> {
+    let result = match read_to_string("/proc/uptime") {
+        Ok(line) => { 
+            info!("{}", line);
+            let mut boot_time_ns = 0.0;
+            sscanf!(&line, "{} {}", boot_time_ns);
+            info!("{}", boot_time_ns);
+            (boot_time_ns * 1000000000.0) as u64
+            // match sscanf!(line, "{f32} {f32}") {
+            //     Ok(ret) => {
+            //         info!("{}", ret.0);
+            //         Ok((ret.0 * 1000000000.0) as u64)
+            //     },
+            //     Err(_) => Err(0),
+            // }
+        },
+        Err(_) => 0,
+    };
+    Ok(result)
+}
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
@@ -37,6 +69,7 @@ async fn main() -> Result<(), anyhow::Error> {
         // This can happen if you remove all log statements from your eBPF program.
         warn!("failed to initialize eBPF logger: {}", e);
     }
+
     // let program: &mut TracePoint = bpf.program_mut("trac").unwrap().try_into()?;
     // program.load()?;
     // program.attach("sched", "sched_switch")?;
@@ -48,12 +81,42 @@ async fn main() -> Result<(), anyhow::Error> {
             PerfTypeId::Hardware,
             perf_hw_id::PERF_COUNT_HW_CPU_CYCLES as u64,
             PerfEventScope::AllProcessesOneCpu { cpu },
-            SamplePolicy::Frequency(10000),
+            SamplePolicy::Frequency(1000),
         )?;
     }
+
+    let mut settings_map = HashMap::try_from(bpf.map_mut("SETTINGS_MAP").unwrap())? as HashMap<&mut MapData, u64, u64>;
+    match boot_time_get_ns() {
+        Ok(boot_time) => {
+            settings_map.insert(START_TIME_KEY, boot_time, 0);
+            settings_map.insert(SAMEPLE_RATE_KEY, 500, 0);
+        },
+        Err(_) => {
+            panic!("failed to get boot time nanoseconds");
+        }
+    }
+
+    let cycles_map = Array::try_from(bpf.map_mut("TOTAL_CYCLES_MAP").unwrap())? as Array<&mut MapData, u64>;
+    let start_time = Instant::now();
+
+    
     info!("Waiting for Ctrl-C...");
     signal::ctrl_c().await?;
     info!("Exiting...");
+
+
+    // TODO: replace 500 by sample rate
+    let num_buckets = (Instant::now().duration_since(start_time).as_millis() / 500) as u32;
+
+
+    for i in 0..num_buckets {
+        let k = cycles_map.get(&i, 0)?;
+        info!("{k}");
+    }
+
+
+    
+
 
     Ok(())
 }

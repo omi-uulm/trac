1732dfa8a9dd84076a69055686f62742cc7cc372
Sascha Winkelhofer <sascha.winkelhofer@gini.net>
2023-12-22 19:50:28 +0100

    added XDP

--- trac-ebpf/src/main.rs
+++ trac-ebpf/src/main.rs
@@ -3,29 +3,41 @@
 
 mod task;
 mod perf_event;
+mod net;
 
 use aya_bpf::{
     helpers::{bpf_perf_prog_read_value, bpf_get_current_task_btf, bpf_ktime_get_boot_ns },
-    macros::{ perf_event, tracepoint, map },
-    bindings::{ bpf_perf_event_value, bpf_perf_event_data },
-    programs::{ PerfEventContext, TracePointContext },
+    macros::{ perf_event, tracepoint, map, xdp },
+    bindings::{ bpf_perf_event_value, bpf_perf_event_data, xdp_action::{XDP_PASS, XDP_ABORTED} },
+    programs::{ PerfEventContext, TracePointContext, XdpContext },
     BpfContext,
     maps::{ PerCpuHashMap, Array, HashMap },
 };
 use aya_log_ebpf::info;
-use core::ptr::addr_of_mut;
+use core::{ptr::addr_of_mut, mem::size_of};
 use task::task_struct;
 use perf_event::bpf_perf_event_data as native_bpf_perf_event_data;
+use net::{xdp_md, ethhdr, iphdr, ipv6hdr, tcphdr, udphdr};
 
 pub struct ProcessPerfCounterEntry {
     pub prev_counter: u64,
     pub proc_counter: u64,
 }
 
+pub struct NettraceSample {
+	pub bytes: u64,
+	pub count: u64,
+}
+
 static START_TIME_KEY: u64 = 0;
 static SAMEPLE_RATE_KEY: u64 = 1;
 static PID_KEY: u64 = 2;
 static MS_IN_NS: u64 = 1000000;
+
+static ETH_P_IP: u16 = (0x0800 as u16).to_be();
+static ETH_P_IPV6: u16 = (0x86DD as u16).to_be();
+static IPPROTO_TCP: u32 = 6;
+static IPPROTO_UDP: u32 = 17;
 
 #[map]
 static SETTINGS_MAP: HashMap<u64, u64> = HashMap::with_max_entries(3, 0);
@@ -35,6 +47,9 @@ static PROC_MAP: PerCpuHashMap<u64, ProcessPerfCounterEntry> = PerCpuHashMap::wi
 
 #[map]
 static TOTAL_CYCLES_MAP: Array<u64> = Array::with_max_entries(262144, 0);
+
+#[map]
+static NETTRACE_MAP: Array<NettraceSample> = Array::with_max_entries(262144, 0);
 
 #[perf_event]
 pub fn observe_cpu_clock(ctx: PerfEventContext) -> u32 {
@@ -72,7 +87,7 @@ fn try_observe_cpu_clock(ctx: PerfEventContext) -> Result<u32, u32> {
     let mut value = bpf_perf_event_value { counter: 0, enabled: 0, running: 0 };
     let value_ptr = addr_of_mut!(value);
 
-    unsafe { bpf_perf_prog_read_value(read_value_ctx as *mut bpf_perf_event_data, value_ptr, core::mem::size_of::<bpf_perf_event_value>() as u32); };
+    unsafe { bpf_perf_prog_read_value(read_value_ctx as *mut bpf_perf_event_data, value_ptr, size_of::<bpf_perf_event_value>() as u32); };
 
     let watch_pid: u64 = match unsafe { SETTINGS_MAP.get(&PID_KEY) } {
         None => 0,
@@ -138,6 +153,100 @@ fn try_trac(ctx: TracePointContext) -> Result<u32, u32> {
     info!(&ctx, "tracepoint sched_switch called");
     Ok(0)
 }
+
+fn try_get_payload_size(ctx: XdpContext) -> u32 {
+    let native_ctx = ctx.as_ptr() as *const xdp_md;
+    let data_start = unsafe { (*native_ctx).data } as u64;
+    let data_end = unsafe { (*native_ctx).data_end } as u64;
+    let total = data_end - data_start;
+    let mut hdr_len: u32;
+    let mut h_proto: u32;
+    let eth = data_start as *const ethhdr;
+
+    hdr_len = size_of::<ethhdr>() as u32;
+
+    if data_start + hdr_len as u64 > data_end {
+        return 0;
+    }
+
+    h_proto = unsafe { (*eth).h_proto } as u32;
+    if h_proto == ETH_P_IP as u32 {
+        if data_start + hdr_len as u64 + size_of::<iphdr>() as u64 > data_end {
+            h_proto = 0;
+        } else {
+            let iph = (data_start + hdr_len as u64) as *const iphdr;
+            h_proto = unsafe { (*iph).protocol } as u32;
+        }
+    } else if h_proto == ETH_P_IPV6 as u32 {
+        if data_start + hdr_len as u64 + size_of::<ipv6hdr>() as u64 > data_end {
+            h_proto = 0;
+        } else {
+            let iph = (data_start + hdr_len as u64) as *const ipv6hdr;
+            h_proto = unsafe { (*iph).nexthdr } as u32;
+        }
+    } else {
+        return 0;
+    }
+
+    if h_proto == IPPROTO_TCP {
+        let tcph: *const tcphdr;
+
+        if data_start + hdr_len as u64 + size_of::<tcphdr>() as u64 > data_end {
+            return 0;
+        }
+
+        tcph = (data_start + hdr_len as u64) as *const tcphdr;
+        let doff = unsafe { (*tcph).doff() } as u64;
+        if doff > 10 || data_start + hdr_len as u64 + doff * 4 > data_end {
+            return 0;
+        }
+
+        hdr_len += (doff * 4) as u32;
+
+        if hdr_len as u64 > total || hdr_len > 100 {
+            return 0;
+        }
+    } else if h_proto == IPPROTO_UDP {
+        if data_start + hdr_len as u64 + size_of::<udphdr>() as u64 > data_end {
+            return 0;
+        }
+
+        hdr_len += size_of::<udphdr>() as u32;
+    } else {
+        return 0
+    }
+
+    return total as u32 - hdr_len;
+}
+
+#[xdp]
+pub fn observe_iface(ctx: XdpContext) -> u32 {
+    match unsafe { try_observe_iface(ctx) } {
+        Ok(ret) => ret,
+        Err(_) => XDP_ABORTED,
+    }
+}
+
+unsafe fn try_observe_iface(ctx: XdpContext) -> Result<u32, u32> {
+    let bucket = get_current_bucket();
+
+    let pkt_len = try_get_payload_size(ctx);
+
+    if pkt_len == 0 {
+        return Err(XDP_PASS);
+    }
+
+    match NETTRACE_MAP.get_ptr_mut(bucket) {
+        None => {},
+        Some(i) => {
+            (*i).bytes += pkt_len as u64;
+            (*i).count += 1;
+        },
+    };
+
+    Ok(XDP_PASS)
+}
+
 
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
--- trac/src/main.rs
+++ trac/src/main.rs
@@ -1,8 +1,8 @@
 use aya::maps::{Array, MapData, HashMap};
 use aya::programs::perf_event::{perf_hw_id, PerfEventScope};
-use aya::programs::{PerfEvent, PerfTypeId, SamplePolicy, TracePoint};
+use aya::programs::{PerfEvent, PerfTypeId, SamplePolicy, TracePoint, Xdp, XdpFlags, Program};
 use aya::util::online_cpus;
-use aya::{include_bytes_aligned, Bpf};
+use aya::{include_bytes_aligned, Bpf, Pod};
 use aya_log::BpfLogger;
 use libc::__c_anonymous_ptrace_syscall_info_entry;
 use log::{debug, info, warn};
@@ -14,10 +14,19 @@ use std::{
     fs::read_to_string,
 };
 use scanf::sscanf;
+use clap::{ Parser, Subcommand };
 
 static START_TIME_KEY: u64 = 0;
 static SAMEPLE_RATE_KEY: u64 = 1;
 static PID_KEY: u64 = 2;
+
+#[derive(Debug, Copy, Clone)]
+pub struct NettraceSample {
+	pub bytes: u64,
+	pub count: u64,
+}
+
+unsafe impl Pod for NettraceSample{ }
 
 pub fn boot_time_get_ns() -> Result<u64, u64> {
     let result = match read_to_string("/proc/uptime") {
@@ -40,14 +49,29 @@ pub fn boot_time_get_ns() -> Result<u64, u64> {
     Ok(result)
 }
 
-#[tokio::main]
-async fn main() -> Result<(), anyhow::Error> {
-    // TODO: temporary, replace by a config file later
-    let args: Vec<String> = args().collect();
-    let pid = &args[1];
-    let pid: u64 = pid.parse().unwrap();
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[clap(short, default_value = "test", global = true)]
+    test: String,
 
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+#[derive(Subcommand)]
+enum Commands {
+    Net {
+        #[clap(short, long, default_value = "eth0")]
+        iface: String,
+    },
+    CPU {
+        #[clap(short, long)]
+        pid: u64,
+    },
+}
 
+fn init() -> Bpf {
     env_logger::init();
 
     // Bump the memlock rlimit. This is needed for older kernels that don't use the
@@ -68,36 +92,33 @@ async fn main() -> Result<(), anyhow::Error> {
     #[cfg(debug_assertions)]
     let mut bpf = Bpf::load(include_bytes_aligned!(
         "../../target/bpfel-unknown-none/debug/trac"
-    ))?;
+    )).unwrap();
     #[cfg(not(debug_assertions))]
     let mut bpf = Bpf::load(include_bytes_aligned!(
         "../../target/bpfel-unknown-none/release/trac"
-    ))?;
+    )).unwrap();
     if let Err(e) = BpfLogger::init(&mut bpf) {
         // This can happen if you remove all log statements from your eBPF program.
         warn!("failed to initialize eBPF logger: {}", e);
     }
+    return bpf
+}
 
-    // let program: &mut TracePoint = bpf.program_mut("trac").unwrap().try_into()?;
-    // program.load()?;
-    // program.attach("sched", "sched_switch")?;
-
-    let program_perf: &mut PerfEvent = bpf.program_mut("observe_cpu_clock").unwrap().try_into()?;
-    program_perf.load()?;
-    for cpu in online_cpus()? {
+async fn handle_cpu(pid: &u64, bpf: &mut Bpf) {
+    let program_perf: &mut PerfEvent = bpf.program_mut("observe_cpu_clock").unwrap().try_into().unwrap();
+    program_perf.load().unwrap();
+    for cpu in online_cpus().unwrap() {
         program_perf.attach(
             PerfTypeId::Hardware,
             perf_hw_id::PERF_COUNT_HW_CPU_CYCLES as u64,
             PerfEventScope::AllProcessesOneCpu { cpu },
             SamplePolicy::Frequency(1000),
-        )?;
+        ).unwrap();
     }
-
-    let mut settings_map = HashMap::try_from(bpf.map_mut("SETTINGS_MAP").unwrap())? as HashMap<&mut MapData, u64, u64>;
+    let mut settings_map = HashMap::try_from(bpf.map_mut("SETTINGS_MAP").unwrap()).unwrap() as HashMap<&mut MapData, u64, u64>;
     match boot_time_get_ns() {
         Ok(boot_time) => {
             settings_map.insert(START_TIME_KEY, boot_time, 0);
-            // TODO: replace 500 by sample rate
             settings_map.insert(SAMEPLE_RATE_KEY, 500, 0);
             settings_map.insert(PID_KEY, pid, 0);
         },
@@ -106,27 +127,71 @@ async fn main() -> Result<(), anyhow::Error> {
         }
     }
 
-    let cycles_map = Array::try_from(bpf.map_mut("TOTAL_CYCLES_MAP").unwrap())? as Array<&mut MapData, u64>;
+    let cycles_map = Array::try_from(bpf.map_mut("TOTAL_CYCLES_MAP").unwrap()).unwrap() as Array<&mut MapData, u64>;
     let start_time = Instant::now();
-
     
     info!("Waiting for Ctrl-C...");
-    signal::ctrl_c().await?;
+    signal::ctrl_c().await.unwrap();
     info!("Exiting...");
-
-
-    // TODO: replace 500 by sample rate
+    
     let num_buckets = (Instant::now().duration_since(start_time).as_millis() / 500) as u32;
-
-
     for i in 0..num_buckets {
-        let k = cycles_map.get(&i, 0)?;
+        let k = cycles_map.get(&i, 0).unwrap();
         info!("{k}");
     }
+}
 
+async fn handle_net(iface: &String, bpf: &mut Bpf) {
+    let program_xdp: &mut Xdp = bpf.program_mut("observe_iface").unwrap().try_into().unwrap();
+    program_xdp.load().unwrap();
+    program_xdp.attach(&iface, XdpFlags::default()).unwrap();
 
-    
+    let mut settings_map = HashMap::try_from(bpf.map_mut("SETTINGS_MAP").unwrap()).unwrap() as HashMap<&mut MapData, u64, u64>;
+    match boot_time_get_ns() {
+        Ok(boot_time) => {
+            settings_map.insert(START_TIME_KEY, boot_time, 0);
+            settings_map.insert(SAMEPLE_RATE_KEY, 500, 0);
+        },
+        Err(_) => {
+            panic!("failed to get boot time nanoseconds");
+        }
+    }
 
+    let nettrace_map = Array::try_from(bpf.map_mut("NETTRACE_MAP").unwrap()).unwrap() as Array<&mut MapData, NettraceSample>;
+    let start_time: Instant = Instant::now();
+
+    info!("Waiting for Ctrl-C...");
+    signal::ctrl_c().await.unwrap();
+    info!("Exiting...");
+
+    let num_buckets = (Instant::now().duration_since(start_time).as_millis() / 500) as u32;
+    for i in 0..num_buckets {
+        let k = nettrace_map.get(&i, 0).unwrap();
+        info!("{}", k.bytes);
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let mut bpf = init();
+
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some( Commands::CPU { pid }) => {
+            handle_cpu(&pid, &mut bpf).await;
+        }
+        Some(Commands::Net { iface }) => {
+            // let mut bpf_xdp = Bpf::load(include_bytes_aligned!(
+            //     "../../target/debug/trac-xdp"
+            // )).unwrap();
+
+            handle_net(&iface, &mut bpf).await;
+        }
+        None => {
+            println!("Default subcommand");
+        }
+    }
 
     Ok(())
 }

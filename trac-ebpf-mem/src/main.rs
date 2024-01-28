e07c468bef3eeb81d9fdf4f59972f7fe1c2fd89c
Sascha Winkelhofer <sascha.winkelhofer@gini.net>
2024-01-28 18:12:22 +0100

    update

--- trac-ebpf-mem/src/main.rs
+++ trac-ebpf-mem/src/main.rs
@@ -8,7 +8,7 @@ use aya_bpf::{
     macros::{ tracepoint, map },
     programs::TracePointContext,
     BpfContext,
-    maps::{ PerCpuArray, PerCpuHashMap },
+    maps::{ PerCpuArray, HashMap },
 };
 use task::task_struct;
 use trac_profiling_macros::{ profiling, profiling_maps_def };
@@ -16,7 +16,7 @@ use trac_common::*;
 use trac_ebpf::bpf_defaults;
 
 #[map]
-static RSS_LAST_STATE_MAP: PerCpuHashMap<i32, RSSStatSample> = PerCpuHashMap::with_max_entries(10000, 0);
+static RSS_LAST_STATE_MAP: HashMap<i32, RSSStatSample> = HashMap::with_max_entries(10000, 0);
 
 #[map]
 static RSS_STAT_MAP: PerCpuArray<[i64; 4]> = PerCpuArray::with_max_entries(262144, 0);
